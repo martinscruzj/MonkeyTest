80f4b1b556469b312e2d98846216a51334c5b3a8
martinscruzj <101901076+martinscruzj@users.noreply.github.com>
2022-03-28 15:28:03 -0700

    Add factorial function

--- monkeycode.cpp
+++ monkeycode.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+int factorial(int n);
+
 int main(int argc, char** argv)
 {
-   cout << "Hello monkeys" << endl;
+   cout << "0 factorial is " << factorial(0) << endl;
+   cout << "4 factorial is " << factorial(4) << endl;
+}
+
+int factorial(int n)
+{
+   int res = 0;
+   if (n > 0) {
+      return n*factorial(n-1);
+   }
+   else{
+      return 1;
+   }
 }
